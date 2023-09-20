12052a37a2046648e3f4d8ab015bbcbd89cbd1bb
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-20 21:28:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+// function upperCaseCustomerName(){
+//     customers = customerName.toUpperCase();
+//     // return customers;
+    
+// }
+function upperCaseCustomerName() {
+    if (customerName) {
+      customerName = customerName.toUpperCase();
+    }
+  }
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+ const LeastFavoriteCustomer = "Joy"
+function changeLeastFavoriteCustomer(){
+ LeastFavoriteCustomer = "Rutto";
+}
